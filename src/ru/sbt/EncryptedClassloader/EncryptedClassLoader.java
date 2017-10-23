24f22513de020b4b87a510dcd9e9fb73804452f7
axess <Exzeest@gmail.com>
2017-10-23 04:06:07 +0300

    Added EncryptedClassLoader

--- src/ru/sbt/EncryptedClassloader/EncryptedClassLoader.java
+++ src/ru/sbt/EncryptedClassloader/EncryptedClassLoader.java
@@ -1,6 +1,8 @@
 package ru.sbt.EncryptedClassloader;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EncryptedClassLoader extends ClassLoader {
     private final String key;
@@ -10,6 +12,42 @@ public class EncryptedClassLoader extends ClassLoader {
         super(parent);
         this.key = key;
         this.dir = dir;
+    }
+
+    @Override
+    public Class<?> loadClass ( String name ) throws ClassNotFoundException {
+
+        Class <?> result = findClass (name);
+        File encryptedClassFile = new File (dir + "\\" + name + ".class");
+        if (result != null) return result;
+        try {
+            byte[] classBytes = loadFileAsBytes (encryptedClassFile);
+            result = defineClass (name, classBytes, 0, classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException ("Cannot load class " + name + ": " + e);
+        } catch (ClassFormatError e) {
+            throw new ClassNotFoundException ("Format of class file incorrect for class " + name + ": " + e);
+        }
+
+        return result;
+    }
+
+    private byte[] loadFileAsBytes ( File file ) throws IOException {
+        byte[] result = new byte[(int) file.length ()];
+        FileInputStream f = new FileInputStream (file);
+        try {
+            f.read (result, 0, result.length);
+        } finally {
+            try {
+                f.close ();
+            } catch (Exception e) {
+            }
+        }
+        // Encryption
+        for (int i = 0 ; i < result.length ; ++i) {
+            result[i] += Byte.valueOf (key);
+        }
+        return result;
     }
 }
 
